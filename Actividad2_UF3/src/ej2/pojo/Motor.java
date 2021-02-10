ee40a016aff07a0799aa72a6fa3ba8b8a880ec57
Alfredo <earp69@msn.com>
2021-02-10 19:44:33 +0100

    Creación de la clase motor con atributos, constructor y metodos de acceso

---
+++ Actividad2_UF3/src/ej2/pojo/Motor.java
@@ -0,0 +1,33 @@
+package ej2.pojo;
+
+public class Motor {
+	private int id;
+	private String tipoMotor;
+	private String potencia;
+	public Motor(int id, String tipoMotor, String potencia) {
+	
+		this.id = id;
+		this.tipoMotor = tipoMotor;
+		this.potencia = potencia;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTipoMotor() {
+		return tipoMotor;
+	}
+	public void setTipoMotor(String tipoMotor) {
+		this.tipoMotor = tipoMotor;
+	}
+	public String getPotencia() {
+		return potencia;
+	}
+	public void setPotencia(String potencia) {
+		this.potencia = potencia;
+	}
+	
+	
+}
