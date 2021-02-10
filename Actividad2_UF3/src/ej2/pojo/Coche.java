ee9174bdcd9aac3d743daaf4d57ae032531e819a
Alfredo <earp69@msn.com>
2021-02-10 18:57:35 +0100

    Creación de WS, y clase coche (pojo) con atributos, constructor, metodos de acceso

---
+++ Actividad2_UF3/src/ej2/pojo/Coche.java
@@ -0,0 +1,50 @@
+package ej2.pojo;
+
+public class Coche {
+	private int id;
+	private String matricula;
+	private String modelo;
+	private String marca;
+	
+	public Coche(int id, String matricula, String modelo, String marca) {
+		this.id = id;
+		this.matricula = matricula;
+		this.modelo = modelo;
+		this.marca = marca;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+}
+
